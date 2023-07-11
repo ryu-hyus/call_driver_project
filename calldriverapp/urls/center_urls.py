50569b4a2fb8214901854a8c26bd8b0b053d67bb
ryu-hyus <gustmd1128@naver.com>
2023-07-11 23:26:43 +0900

    front layout

--- calldriverapp/urls/center_urls.py
+++ calldriverapp/urls/center_urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
 urlpatterns = [
-    
+    path("superuserlogin/", SuperuserLoginView.as_view(), name="superuser_login"),
+    path("customerlist/", CustomerListView.as_view(), name="customer_list_view"),
+    path("realtimeorder/", RealtimeOrderView.as_view(), name="realtime_order_list"),
+    path("todayorder/", TodayOrderView.as_view(), name="today_order_list"),
+    path("totalorder/", TotalOrderView.as_view(), name="total_order_list"),
 ]
