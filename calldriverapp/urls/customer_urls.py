6d30781a3458d022731d7765332ef0359c208996
soyeon <soyun6248@naver.com>
2023-07-12 14:07:23 +0900

    directory

--- calldriverapp/urls/customer_urls.py
+++ calldriverapp/urls/customer_urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-
+#
 urlpatterns = [
-    path("signup/", SigUpView.as_view(), name="sign_up"),
-    path("customerlogin/", CustomerLoginView.as_view(), name="customer_login"),
-    path("customerinfo/", CustomerInfoView.as_view(), name="customer_information"),
-    path("getorder/", GetOrderView.as_view(), name="get_order"),
-    path("confirmorder/", ConfirmOrderView.as_view(), name="confirm_order"),
-    path("changeorder/", ChangeOrderView.as_view(), name="change_order"),
-    # 주소는 api 연결?
-    path("setaddress/", SetAddressView.as_view(), name="set_adress"),
-    path("pricetable/", PriceTableView.as_view(), name="price_table"),
-]
+#     path("signup/", SigUpView.as_view(), name="sign_up"),
+#     path("customerlogin/", CustomerLoginView.as_view(), name="customer_login"),
+#     path("customerinfo/", CustomerInfoView.as_view(), name="customer_information"),
+#     path("getorder/", GetOrderView.as_view(), name="get_order"),
+#     path("confirmorder/", ConfirmOrderView.as_view(), name="confirm_order"),
+#     path("changeorder/", ChangeOrderView.as_view(), name="change_order"),
+#     # 주소는 api 연결?
+#     path("setaddress/", SetAddressView.as_view(), name="set_adress"),
+#     path("pricetable/", PriceTableView.as_view(), name="price_table"),
+ ]
--- calldriverapp/views/center.py
+++ calldriverapp/views/center.py
@@ -7,17 +7,20 @@ from django.views import View
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 
-class SuperuserLoginView(TemplateView):
-    template_name = "superuser_login_template.html"  
+class MainView(TemplateView):
+    template_name = "center/layout/left_list.html"
 
-class CustomerListView(TemplateView):
-    template_name = "customer_list_template.html"
+# class SuperuserLoginView(TemplateView):
+#     template_name = "superuser_login_template.html"  
 
-class RealtimeOrderView(TemplateView):
-    template_name = "realtime_order_template.html"
+# class CustomerListView(TemplateView):
+#     template_name = "customer_list_template.html"
 
-class TodayOrderView(TemplateView):
-    template_name = "today_order_template.html"
+# class RealtimeOrderView(TemplateView):
+#     template_name = "realtime_order_template.html"
 
-class TotalOrderView(TemplateView):
-    template_name = "total_order_template.html"
+# class TodayOrderView(TemplateView):
+#     template_name = "today_order_template.html"
+
+# class TotalOrderView(TemplateView):
+#     template_name = "total_order_template.html"
