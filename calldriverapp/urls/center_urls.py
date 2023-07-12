0d14b87ac08031474b5d9989b10a0a815c2b51c2
soyeon <soyun6248@naver.com>
2023-07-12 11:39:30 +0900

    file_order

--- calldriverapp/urls/center_urls.py
+++ calldriverapp/urls/center_urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from calldriverapp.views.center import SuperuserLoginView
+from calldriverapp.views.center import CustomerListView
+from calldriverapp.views.center import RealtimeOrderView
+from calldriverapp.views.center import TodayOrderView
+from calldriverapp.views.center import TotalOrderView
+
 urlpatterns = [
     path("superuserlogin/", SuperuserLoginView.as_view(), name="superuser_login"),
     path("customerlist/", CustomerListView.as_view(), name="customer_list_view"),
