f8c70a9cf0d0b6ca37b60f2d4641df2f8c5cc0ce
soyeon <soyun6248@naver.com>
2023-07-13 15:22:56 +0900

    hahaha

--- calldriverapp/urls/customer_urls.py
+++ calldriverapp/urls/customer_urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from calldriverapp.views import HomeTemplateView
+from calldriverapp.views.customer import HomeTemplateView, SigUpView, CustomerLoginView
 from calldriverapp import views
 
 urlpatterns = [
-    path("signup/", views.SigUpView, name="sign_up"),
-    path("customerlogin/", views.CustomerLoginView, name="customer_login"),
+    path("signup/", SigUpView.as_view(), name="sign_up"),
+    path("customerlogin/", CustomerLoginView.as_view(), name="customer_login"),
     path("home/", HomeTemplateView.as_view(), name="home")
 ]
--- calldriverapp/views/customer.py
+++ calldriverapp/views/customer.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class HomeTemplateView(TemplateView):
     template_name = "customer/index.html"
+
+class CustomerLoginView(TemplateView):
+    template_name = "customer/login.html"
+
+class SigUpView(TemplateView):
+    template_name = "customer/signup.html"
