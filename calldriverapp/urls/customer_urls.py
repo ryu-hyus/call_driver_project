c60f0f141420685936a98f48389c5ae753aeb6d5
ryu-hyus <gustmd1128@naver.com>
2023-07-13 15:57:16 +0900

    make ordermain page

--- calldriverapp/urls/customer_urls.py
+++ calldriverapp/urls/customer_urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from calldriverapp.views import HomeTemplateView
-from calldriverapp import views
+from calldriverapp.views.customer import HomeTemplateView
+# from calldriverapp import views
+from calldriverapp.views.customer import OrderMainTemplateView
 
 urlpatterns = [
-    path("signup/", views.SigUpView, name="sign_up"),
-    path("customerlogin/", views.CustomerLoginView, name="customer_login"),
-    path("home/", HomeTemplateView.as_view(), name="home")
+    # path("signup/", views.SigUpView, name="sign_up"),
+    # path("customerlogin/", views.CustomerLoginView, name="customer_login"),
+    path("home/", HomeTemplateView.as_view(), name="home"),
+    path("ordermain/", OrderMainTemplateView.as_view(), name= "order_main"),
+    path("orderdetail/", OrderMainTemplateView.as_view(), name= "order_detail"),
 ]
