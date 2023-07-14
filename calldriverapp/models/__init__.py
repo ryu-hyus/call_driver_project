0df2c46a3b4dc938228e4f60c7305b2528c7af31
ryu-hyus <gustmd1128@naver.com>
2023-07-15 03:46:42 +0900

    price calculate complete

--- calldriverapp/models/__init__.py
+++ calldriverapp/models/__init__.py
@@ -3,3 +3,4 @@ from .operation import OperationDay, OperationOnOff
 from .orderdata import OrderData
 from .pricetable import PriceTable
 from .customer import MyUser
+from .pricefile import PriceFile
