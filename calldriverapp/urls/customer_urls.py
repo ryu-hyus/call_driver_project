ee02e499d8b63a40c7ea07b039dd47822d71c895
ryu-hyus <gustmd1128@naver.com>
2023-07-14 22:14:15 +0900

    create orderdetail page

--- calldriverapp/urls/customer_urls.py
+++ calldriverapp/urls/customer_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from calldriverapp.views.customer import HomeTemplateView
+from calldriverapp.views.customer import HomeTemplateView, OrderDetailTemplateView
 # from calldriverapp import views
 from calldriverapp.views.customer import OrderMainTemplateView
 
@@ -8,5 +8,5 @@ urlpatterns = [
     # path("customerlogin/", views.CustomerLoginView, name="customer_login"),
     path("home/", HomeTemplateView.as_view(), name="home"),
     path("ordermain/", OrderMainTemplateView.as_view(), name= "order_main"),
-    path("orderdetail/", OrderMainTemplateView.as_view(), name= "order_detail"),
+    path("orderdetail/", OrderDetailTemplateView.as_view(), name= "order_detail"),
 ]
