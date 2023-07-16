f83dbd55ce633eeaa7791cea9b9747e3ce23de89
soyeon <soyun6248@naver.com>
2023-07-16 15:19:12 +0900

    gg

--- calldriverapp/urls/center_urls.py
+++ calldriverapp/urls/center_urls.py
@@ -3,15 +3,15 @@ from django.urls import path
 from calldriverapp.views.center import MainView
 # from calldriverapp.views.center import SuperuserLoginView
 # from calldriverapp.views.center import CustomerListView
-# from calldriverapp.views.center import RealtimeOrderView
-# from calldriverapp.views.center import TodayOrderView
+from calldriverapp.views.center import RealtimeOrderView
+from calldriverapp.views.center import TodayOrderView
 # from calldriverapp.views.center import TotalOrderView
 
 urlpatterns = [
     path("main/", MainView.as_view(), name="center_main"),
     # path("superuserlogin/", SuperuserLoginView.as_view(), name="superuser_login"),
     # path("customerlist/", CustomerListView.as_view(), name="customer_list_view"),
-    # path("realtimeorder/", RealtimeOrderView.as_view(), name="realtime_order_list"),
-    # path("todayorder/", TodayOrderView.as_view(), name="today_order_list"),
+    path("realtimeorder/", RealtimeOrderView.as_view(), name="realtime_order_list"),
+    path("todayorder/", TodayOrderView.as_view(), name="today_order_list"),
     # path("totalorder/", TotalOrderView.as_view(), name="total_order_list"),
 ]
--- calldriverapp/views/center.py
+++ calldriverapp/views/center.py
@@ -11,16 +11,16 @@ class MainView(TemplateView):
     template_name = "center/home.html"
 
 # class SuperuserLoginView(TemplateView):
-#     template_name = "superuser_login_template.html"  
+#     template_name = "center/superuser_login_template.html"  
 
 # class CustomerListView(TemplateView):
-#     template_name = "customer_list_template.html"
+#     template_name = "center/customer_list_template.html"
 
-# class RealtimeOrderView(TemplateView):
-#     template_name = "realtime_order_template.html"
+class RealtimeOrderView(TemplateView):
+    template_name = "center/realtime_order_template.html"
 
-# class TodayOrderView(TemplateView):
-#     template_name = "today_order_template.html"
+class TodayOrderView(TemplateView):
+    template_name = "center/today_order_template.html"
 
 # class TotalOrderView(TemplateView):
-#     template_name = "total_order_template.html"
+#     template_name = "center/total_order_template.html"
