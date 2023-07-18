08cd1eed73dfc7b8537014f349106d8aa5dc3d40
qkdrnehd-wnals <rhrh037@naver.com>
2023-07-18 10:21:45 +0900

    c

--- calldriverapp/urls/customer_urls.py
+++ calldriverapp/urls/customer_urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from calldriverapp.views.addresshistoryCRUD import AddresshistorydataView, AddresshistorydeleteView
 from calldriverapp.views.customer import HomeTemplateView, OrderDetailTemplateView, OrderDetailTemplateView
 from calldriverapp.views.customer import OrderMainTemplateView
-from calldriverapp.views.member_info import CustomerLoignView, SignUpView
+from calldriverapp.views.member_info import CustomerLoignView, FindIdView, SignUpView
 from calldriverapp.views.customer import OrderMainTemplateView, OrderChangeTemplateView
 from calldriverapp.views.orderdataCRUD import OrderdataView, CustomerOrderView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="sign_up"),
     path("login/", CustomerLoignView.as_view(), name="customer_login"),
+    path("find_id/", FindIdView.as_view(), name="find_id"),
     path("home/", HomeTemplateView.as_view(), name="home"),
     path("ordermain/", OrderMainTemplateView.as_view(), name= "order_main"),
     path("orderdetail/", OrderDetailTemplateView.as_view(), name= "order_detail"),
--- calldriverapp/views/member_info.py
+++ calldriverapp/views/member_info.py
@@ -1,5 +1,5 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate
 from django.views import View
@@ -9,6 +9,7 @@ from calldriverapp.models.customer import MyUser
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth import login as login
 from django.contrib.auth.hashers import make_password
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CustomerLoignView(View):
@@ -24,7 +25,8 @@ class CustomerLoignView(View):
             if user is not None:
                 login(request, user)
                 if check_password(password, user.password):
-                    return render(request, 'customer/ordermain.html')  # 로그인 성공시 연결 
+                    return render(request, 'customer/index.html')  # 로그인 성공시 연결            
+            return HttpResponse("유저가 없습니다", status=500)
         else:
             return render(request, 'customer/login.html')
 
@@ -48,11 +50,46 @@ class SignUpView(View):
             if not username:
                 return HttpResponse('아이디를 입력해주세요.', status=400)
             
+            password = phone_number[-4: ]
             hashed_password = make_password(password)
-
             user = MyUser.objects.create_user(username=username, password=password, phone_number=phone_number, gear_type=gear_type)
             user.name = name
             user.save()
             return redirect("/customer/login/")
 
         return render(request, "customer/signup.html")
+    
+
+@method_decorator(csrf_exempt, name='dispatch')
+class StaffLoginView(View):
+    def get(self, request):
+        return render(request,'center/login.html')
+
+    def post(self, request):
+        if request.method == "POST":
+            data = json.loads(request.body)
+            username = data.get("username")
+            password = data.get("password") 
+            user = authenticate(request, username=username, password=password)
+            if user is not None and user.is_staff:
+                login(request, user)
+                if check_password(password, user.password):
+                    return render(request, 'customer/index.html')  # 로그인 성공시 연결            
+            return HttpResponse("유저가 없습니다", status=500)
+        else:
+            return render(request, 'customer/login.html')      
+            
+@method_decorator(csrf_exempt, name='dispatch')
+class FindIdView(View):
+    def get(self, request):
+        return render(request, 'customer/find_id.html')
+    
+    def post(self, request):
+        name = request.POST.get('name')
+        phone_number = request.POST.get('phone_number')
+
+        try:
+            user = MyUser.objects.get(name=name, phone_number=phone_number)
+            return JsonResponse({ "username" : user.username })
+        except MyUser.DoesNotExist:
+            return JsonResponse({ "error" : "아이디를 찾을 수 없습니다."}, status=400 )
