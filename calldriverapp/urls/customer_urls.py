1e64598e32d59b85384c0322b9b70fac13b99597
ryu-hyus <gustmd1128@naver.com>
2023-07-19 10:44:26 +0900

    error fix

--- calldriverapp/urls/customer_urls.py
+++ calldriverapp/urls/customer_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from calldriverapp.views.addresshistoryCRUD import AddresshistorydataView, AddresshistorydeleteView
-from calldriverapp.views.customer import HomeTemplateView, MapExamView, OrderDetailTemplateView, OrderDetailTemplateView, SearchAddress
+from calldriverapp.views.customer import HomeTemplateView, OrderDetailTemplateView, OrderDetailTemplateView
 
 from calldriverapp.views.customer import OrderMainTemplateView
 from calldriverapp.views.member_info import CustomerLoignView, FindIdView, MyPageView, SignUpView, update_profile
@@ -27,7 +27,5 @@ urlpatterns = [
     path("orderdata/customer/<int:pk>/", CustomerOrderView.as_view(), name= "customer_order_method_select"), #커스토머 id로 데이터 조회 
     path("addresshistory/<int:pk>/", AddresshistorydataView.as_view(), name= "addresshistory_CRUD"), #커스토머 id로 주소 검색 기록 검색/등록
     path("addresshistory/delete/<int:pk>/", AddresshistorydeleteView.as_view(), name= "addresshistory_delete"), #히스토리 id로 삭제
-    path("mapexam/", MapExamView.as_view(), name= "map_exam"),
-    path("searchaddress/", SearchAddress.as_view(), name= "search_address"),
     path("pricecalculate/<str:ss>/<str:es>/", PriceTableView.as_view(), name= "calculate_price"), # 요금 계산 url / ss: 출발지, es: 도착지로 검색
 ]
