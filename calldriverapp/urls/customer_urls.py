1b8f13022aced4da986cf7f6e2621c089b045208
qkdrnehd-wnals <rhrh037@naver.com>
2023-07-21 00:13:41 +0900

    g

--- calldriverapp/urls/customer_urls.py
+++ calldriverapp/urls/customer_urls.py
@@ -3,7 +3,7 @@ from calldriverapp.views.addresshistoryCRUD import AddresshistorydataView, Addre
 from calldriverapp.views.customer import HomeTemplateView, OrderDetailTemplateView, OrderDetailTemplateView, SearchAddressView
 
 from calldriverapp.views.customer import OrderMainTemplateView
-from calldriverapp.views.member_info import CustomerLoignView, FindIdView, MyPageView, SignUpView, update_profile
+from calldriverapp.views.member_info import CustomerLoignView, FindIdView, Logout, MyPageView, SignUpView, update_profile
 from calldriverapp.views.customer import OrderMainTemplateView, OrderChangeTemplateView, PricePageView
 from calldriverapp.views.orderdataCRUD import OrderdataView, CustomerOrderView
 from calldriverapp.views.pricecaculate import PriceTableView
@@ -13,10 +13,12 @@ from calldriverapp.views.myuserCRUD import CustomerInfoView
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="sign_up"),
     path("login/", CustomerLoignView.as_view(), name="customer_login"),
+    path("logout/", Logout, name="customer_login"),
     path("find_id/", FindIdView.as_view(), name="find_id"),
     path("home/", HomeTemplateView.as_view(), name="home"),
     path("mypage/", MyPageView.as_view(), name="mypage"),
     path("mypage/update_profile/", update_profile, name="update_profile"),
+    path("customer_info/", CustomerInfoView.as_view(), name="customer_info_select"),
     path("customer_info/<int:pk>/", CustomerInfoView.as_view(), name="customer_info_select"),
     path("ordermain/", OrderMainTemplateView.as_view(), name= "order_main"),
     path("orderdetail/", OrderDetailTemplateView.as_view(), name= "order_detail"),
