81a2e0bc7cdb6c9a095e6d2641a0f97282dc97fe
jurijung <ubbest@naver.com>
2023-07-25 11:33:18 +0900

    mysql db setup

--- call_driver_project/settings.py
+++ call_driver_project/settings.py
@@ -85,9 +85,20 @@ CSRF_COOKIE_HTTPONLY = False
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CALLDRIVER_DB',
+        'USER': 'calldriver',
+        'PASSWORD': 'calldriver202307*',
+        'HOST': '43.200.169.248',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
