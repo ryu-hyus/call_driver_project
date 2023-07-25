c8b4495f92145de7ac1ee5b98039a1901078fdc6
ryu-hyus <gustmd1128@naver.com>
2023-07-25 14:39:31 +0900

    devsetting

--- call_driver_project/settings.py
+++ call_driver_project/settings.py
@@ -85,21 +85,21 @@ CSRF_COOKIE_HTTPONLY = False
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CALLDRIVER_DB',
-        'USER': 'call_driver',
-        'PASSWORD': 'calldriver2023*',
-        'HOST': '3.39.61.233',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-        }
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'CALLDRIVER_DB',
+    #     'USER': 'call_driver',
+    #     'PASSWORD': 'calldriver2023*',
+    #     'HOST': '3.39.61.233',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #         'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+    #     }
+    # }
 }
 
 
