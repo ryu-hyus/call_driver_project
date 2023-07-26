b2b54c1f25e7c569187bb9a7321b9906821343f4
ryu-hyus <gustmd1128@naver.com>
2023-07-26 11:45:01 +0900

    operationday& mainpage

--- calldriverapp/urls/center_urls.py
+++ calldriverapp/urls/center_urls.py
@@ -5,7 +5,7 @@ from calldriverapp.views.center import RealtimeOrderView
 from calldriverapp.views.center import TodayOrderView
 from calldriverapp.views.center import TotalOrderView
 from calldriverapp.views.member_info import StaffLoginView
-from calldriverapp.views.operationCRUD import OperationOnOffView
+from calldriverapp.views.operationCRUD import OperationDaySetView, OperationOnOffView
 from calldriverapp.views.realtimeorder import RealtimeOrderGetView
 from calldriverapp.views.todayorderCRUD import TodayOrderGetView
 from calldriverapp.views.totalorderCRUD import TotalOrderGetView
@@ -17,6 +17,7 @@ urlpatterns = [
     path("todayorder/", TodayOrderView.as_view(), name="today_order_list"),
     path("totalorder/", TotalOrderView.as_view(), name="total_order_list"),
     path("operationonoff/", OperationOnOffView.as_view(), name="operation_onoff"),
+    path("operationdayset/", OperationDaySetView.as_view(), name="operation_dayset"),
     path("offmain/", OffMainView.as_view(), name="center_main"),
     path("offcustomerlist/", OffCustomerView.as_view(), name="off_customer_list"),
     path("offtotalorder/", OffTotalOrderView.as_view(), name="off_total_order_list"),
