1afa75fe561dc99727f4a9c9b081bc0ad057eab3
ryu-hyus <gustmd1128@naver.com>
2023-07-27 17:54:23 +0900

    cach error

--- call_driver_project/settings.py
+++ call_driver_project/settings.py
@@ -28,6 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1","jejuemo.com"]
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Application definition
 
